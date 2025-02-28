0c41bb492819deec3f2c986a27430c9cb6ef07f6
Miguel Riquelme <miguelrisquelme@gmail.com>
2025-02-27 22:16:02 -0300

    feat: adding the toolkit and typescript feats

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
 import "./index.css";
 import App from "./App.tsx";
+
 import { Provider } from "react-redux";
-import { store } from "../redux-tools.ts";
+import { store } from "./store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
